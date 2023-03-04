a4eb4b90c159573624defb8dcbbf172f82e0e074
Zachary <zachary@ZipcodersMBP12.fios-router.home>
2023-03-04 09:52:16 -0500

    I have made changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args){
@@ -8,19 +10,23 @@ public class Main {
         int number = getInputNumber();
         
         int sum = sumOfNumbers(number);
-        
+
         System.out.println(sum);
 
     }
     
     // do this one and the lab is complete. really.
     // return the sum of 0 to n... if n == 3, the result should be 6
-    int sumOfNumbers(int n) {
-        return 0;
-    }
+    static int sumOfNumbers(int n) {
+            int sum = 0;
+            for(int i=1;i <= n;i++) {
+                sum += i;
+                }
+        return sum;
+            }
+            
     
-    
-    int getInputNumber() {
+    static int getInputNumber() {
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter any number: ");
 
@@ -30,10 +36,11 @@ public class Main {
         // Closing Scanner after the use
         scan.close();
         return num;
+
+
     }
-    
-    
-    
+
+
     // for Extra Credit
     int gaussianSumOfNumbers(int n) {
         return 0;
